dc7ec5652f0d8d28f1426622cd11848517d13a87
kalouleloup <lucaspalu866@gmail.com>
2025-02-16 20:41:49 +0100

    add a level selector scene

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelSelector : MonoBehaviour
+{
+    public void LoadLevelPassed(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
