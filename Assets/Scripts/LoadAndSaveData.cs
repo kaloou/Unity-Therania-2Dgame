dd09b52bd3b061974d66332f80e0c8acee661cef
kalouleloup <lucaspalu866@gmail.com>
2025-02-23 01:57:32 +0100

    add save system trough scenes + clear data settings

--- Assets/Scripts/LoadAndSaveData.cs
+++ Assets/Scripts/LoadAndSaveData.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+
+using System.Linq;
 public class LoadAndSaveData : MonoBehaviour
 {
     public static LoadAndSaveData instance;
@@ -15,6 +17,20 @@ public class LoadAndSaveData : MonoBehaviour
     {
         Inventory.instance.coinsCount = PlayerPrefs.GetInt("coinsCount", 0);
         Inventory.instance.UpdateTextUI();
+
+        string[] itemsSaved = PlayerPrefs.GetString("inventoryItems", "").Split(',');
+
+        for (int i = 0; i < itemsSaved.Length; i++)
+        {
+            if(itemsSaved[i] != "")
+            {
+                int id = int.Parse(itemsSaved[i]);
+                Item currentItem = ItemsDatabase.instance.allItems.Single(x => x.id == id);
+                Inventory.instance.content.Add(currentItem);
+            }
+        }
+
+        Inventory.instance.UpdateInventoryUI();
        
     }
     public void SaveData()
@@ -26,6 +42,9 @@ public class LoadAndSaveData : MonoBehaviour
             PlayerPrefs.SetInt("levelReached", CurrentSceneManager.instance.levelToUnlock);
             //Debug.Log("passed1");
         }
+
+        string itemsInInventory = string.Join(",", Inventory.instance.content.Select(x => x.id));
+        PlayerPrefs.SetString("inventoryItems", itemsInInventory);
         //Debug.Log("passed2");
     }
 }
